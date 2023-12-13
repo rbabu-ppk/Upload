b776aefc78b475e2ab36109d7b6ac95757bb82e4
rbabu-ppk <rbabu@ppkvirtueservices.com>
2023-12-13 16:38:35 +0530

    department.ts

--- backend/functions/department.ts
+++ backend/functions/department.ts
@@ -1,30 +1,33 @@
 import csvParser from "csv-parser";
 import { Departments, Sites } from "../types/adminTypes";
-import siteModel from "../models/siteModel";
 import departmentModel from "../models/departmentModel";
 
 export const departmentFunction = async (uploadedFile: any) => {
   const departments: Departments[] = [];
+  const dbDepartments: Record<string, Departments> = {};
   const csvData = uploadedFile.buffer.toString("utf8");
+
+  const dbRecords = await departmentModel.find({});
+  dbRecords.forEach((dept) => {
+    dbDepartments[dept.departmentName] = dept;
+  });
 
   const parseStream = csvParser()
     .on("data", async (data) => {
       const departmentName = data.departmentName;
       const departmentId = data.departmentId;
-      departments.push({ departmentName, departmentId });
+      const department: Departments = { departmentId, departmentName };
+
+      if (!dbDepartments[departmentName]) {
+        dbDepartments[departmentName] = department;
+        departments.push(department);
+      } else {
+        console.log("Record exists");
+      }
     })
     .on("end", async () => {
       try {
-        for (const department of departments) {
-          const existingDepartment = await departmentModel.findOne({
-            departmentName: department.departmentName,
-          });
-          if (!existingDepartment) {
-            await departmentModel.create(department);
-          } else {
-            console.log("available");
-          }
-        }
+        await departmentModel.insertMany(departments);
       } catch (error) {
         console.log(error);
       }
