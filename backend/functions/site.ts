26207e961b7e4221cbafdce2f0bff1317dd69882
rbabu-ppk <rbabu@ppkvirtueservices.com>
2023-12-13 16:43:50 +0530

    site.ts

--- backend/functions/site.ts
+++ backend/functions/site.ts
@@ -4,28 +4,30 @@ import siteModel from "../models/siteModel";
 
 export const siteFunction = async (uploadedFile: any) => {
   const sites: Sites[] = [];
+  const dbSites: Record<string, Sites> = {};
   const csvData = uploadedFile.buffer.toString("utf8");
+
+  const dbRecords = await siteModel.find({});
+  dbRecords.forEach((site) => {
+    dbSites[site.siteName] = site;
+  });
 
   const parseStream = csvParser()
     .on("data", async (data) => {
       const siteName = data.siteName;
       const siteId = data.siteId;
-      sites.push({ siteName, siteId });
+      const site: Sites = { siteId, siteName };
+
+      if (!dbSites[siteName]) {
+        dbSites[siteName] = site;
+        sites.push(site);
+      } else {
+        console.log("Record Exists");
+      }
     })
     .on("end", async () => {
       try {
-        console.log(sites);
-
-        for (const site of sites) {
-          const existingSite = await siteModel.find({
-            siteName: site.siteName,
-          });
-          if (!existingSite) {
-            await siteModel.create(site);
-          } else {
-            console.log("available");
-          }
-        }
+        await siteModel.insertMany(sites);
       } catch (error) {
         console.log(error);
       }
