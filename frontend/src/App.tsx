e46b6a4504b7393f814021c9e76d7d8e94613a8a
rbabu-ppk <rbabu@ppkvirtueservices.com>
2023-12-14 12:48:17 +0530

    added responsiveness using Ant

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Job from "./components/File";
-// import Job from "./components/Upload";
 
 function App() {
   return (
--- frontend/src/components/File.tsx
+++ frontend/src/components/File.tsx
@@ -1,5 +1,15 @@
 import React from "react";
-import { Form, Input, Select, Button, Upload, message, Typography } from "antd";
+import {
+  Form,
+  Input,
+  Select,
+  Button,
+  Upload,
+  message,
+  Typography,
+  Col,
+  Row,
+} from "antd";
 import { UploadOutlined } from "@ant-design/icons";
 import axios from "axios";
 
@@ -41,48 +51,62 @@ const Job: React.FC = () => {
   };
 
   return (
-    <div>
-      <Typography.Title level={2}>File Upload</Typography.Title>
+    <Row justify="center">
+      {/* <Col xs={24} sm={20} md={16} lg={24} xl={24}> */}
+      <Col xs={24} sm={24} md={20} lg={20} xl={20}>
+        <div
+          style={{
+            background: "#ffffff",
+            padding: "80px",
+            borderRadius: "8px",
+            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+          }}
+        >
+          <Typography.Title level={3}>File Upload</Typography.Title>
 
-      <Form
-        form={form}
-        name="upload"
-        onFinish={onFinish}
-        initialValues={{ selectedOption: "Select Type" }}
-      >
-        <Form.Item label="Email" name="email" rules={[{ required: true }]}>
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Type"
-          name="selectedOption"
-          rules={[{ required: true }]}
-        >
-          <Select>
-            <Option value="Select Type">Select Type</Option>
-            <Option value="Department">Department</Option>
-            <Option value="Site">Site</Option>
-            <Option value="Employee">Employee</Option>
-          </Select>
-        </Form.Item>
-        <Form.Item
-          label="File"
-          name="file"
-          valuePropName="fileList"
-          getValueFromEvent={normFile}
-          rules={[{ required: true, message: "Please upload a file" }]}
-        >
-          <Upload beforeUpload={() => false} maxCount={1}>
-            <Button icon={<UploadOutlined />}>Click to Upload</Button>
-          </Upload>
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Upload
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
+          <Form
+            form={form}
+            name="upload"
+            onFinish={onFinish}
+            initialValues={{ selectedOption: "Select Type" }}
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+          >
+            <Form.Item label="Email" name="email" rules={[{ required: true }]}>
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Type"
+              name="selectedOption"
+              rules={[{ required: true }]}
+            >
+              <Select>
+                <Option value="Select Type">Select Type</Option>
+                <Option value="Department">Department</Option>
+                <Option value="Site">Site</Option>
+                <Option value="Employee">Employee</Option>
+              </Select>
+            </Form.Item>
+            <Form.Item
+              label="File"
+              name="file"
+              valuePropName="fileList"
+              getValueFromEvent={normFile}
+              rules={[{ required: true, message: "Please upload a file" }]}
+            >
+              <Upload beforeUpload={() => false} maxCount={1}>
+                <Button icon={<UploadOutlined />}>Click to Upload</Button>
+              </Upload>
+            </Form.Item>
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button type="primary" htmlType="submit">
+                Upload
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </Col>
+    </Row>
   );
 };
 
